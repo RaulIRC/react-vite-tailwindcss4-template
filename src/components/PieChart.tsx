03221da11a7ce523eceaaa5dfa4d384f9ed023d7
NaCat5 <system.ruin@gmail.com>
2025-04-12 13:22:05 -0500

    Improve chart visuals, fix minor data issues

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,3 +1,10 @@
+/*
+PieChart.tsx
+Piechart component to show expense breakdown by category.
+Does not show positive records or "income".
+Data is set by using a listener to get logged in user data.
+*/
+
 import { useState, useEffect } from "react";
 import {
     FinancialRecord,
@@ -9,20 +16,33 @@ import { Pie } from 'react-chartjs-2'
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const ChartComponent: React.FC = () => {
+    // chart component initialized types, separate default data and setData
     const { records } = useFinancialRecords();
     const [data, setData] = useState({
         labels: [] as string[],
         datasets: [
             {
                 data: [] as number[],
+                backgroundColor: [] as string[],
+                borderWidth: 0,
             }
-        ]
+        ],
     });
 
+    // listener to get saved data
     useEffect(() => {
         const categoryMap: Record<string, number> = {};
         records.forEach((record: FinancialRecord) => {
-            const category = record.category || "Unknown";
+
+            // keeping "salary" category / income, do not show positive expense in chart
+            if(record.amount > 0) {
+                return;
+            }
+
+            // either a saved category or "Other" catch all
+            const category = record.category || "Other";
+
+            // add the amount to the category
             if (categoryMap[category]) {
                 categoryMap[category] += record.amount || 0;
             } else {
@@ -30,11 +50,21 @@ const ChartComponent: React.FC = () => {
             }
         });
 
+        // change data in piechart
         setData({
             labels: Object.keys(categoryMap),
             datasets: [
                 {
                     data: Object.values(categoryMap),
+                    backgroundColor: [
+                        "rgb(67, 123, 255)",
+                        "rgb(111, 0, 152)",
+                        "rgb(13, 32, 119)",
+                        "rgb(107, 1, 182)",
+                        "rgb(94, 111, 223)",
+                        "rgb(187, 0, 255)"
+                    ],
+                    borderWidth:1
                 }
             ]
         });
