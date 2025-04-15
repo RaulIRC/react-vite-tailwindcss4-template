ef43ea77d26cdbe4c90a705ceabfd17abf72729d
RaulIRC <raulirodriguezcruz@gmail.com>
2025-04-15 12:22:03 -0500

    idc

---
+++ src/components/loader/LoaderComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoaderComponent = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-gray-100">
+      <span className="loading loading-bars loading-xl"></span>
+    </div>
+  )
+}
+
+export default LoaderComponent
