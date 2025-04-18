3b0774c114b3c50b9828373a3b21dfe12dc4de0c
RaulIRC <raulirodriguezcruz@gmail.com>
2025-04-18 13:48:33 -0500

    Implementing HeroUI into the project.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import NavbarComponent from '../components/navbar/NavbarComponent';
 import Analytics from '../components/analytics/analytics';
-import AuthRoute from '../components/auth/auth';
+import { HeroUIProvider } from "@heroui/react"
 
 // Making this the nav bar that is the drawer 
 //to always have it available while loading each page would be a cool idea.
@@ -17,6 +17,7 @@ function rootNav() {
 
   return (
     <>
+      <HeroUIProvider>
         <div className="bg-[url('cloudy.svg')] bg-cover bg-center min-h-screen">
           {/* Navbar can go here */}
           <header className="navbar-container sticky top-0 z-50 bg-base-100 p-4 gap-2">
@@ -32,6 +33,7 @@ function rootNav() {
           {/* <TanStackRouterDevtools /> */}
           <Analytics />
         </div>
+      </HeroUIProvider>
     </>
   )
 }
